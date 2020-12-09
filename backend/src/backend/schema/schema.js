4fc2a08e0aa0406c639f9f45d33051d413012ed5
Siddharth Patel <siddharth.github@gmail.com>
2020-12-08 20:52:15 -0500

    UI changes for login page

--- backend/src/backend/schema/schema.js
+++ backend/src/backend/schema/schema.js
@@ -60,7 +60,7 @@ const get_data = async url => {
     try {
         const response = await fetch(url);
         const json = await response.json();
-        console.log(json)
+        // console.log(json)
         return json
     } catch (error) {
         console.log(error);
--- plantify-frontend/src/app/header/header.component.ts
+++ plantify-frontend/src/app/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated = false;
+  loggedInUser: User;
+  email: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      if (this.isAuthenticated) {
+        this.email = user.email;
+      }
+      console.log(user);
+    });
+  }
+  onLogout() {
+    this.authService.logout();
   }
 
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
